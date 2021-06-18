1d45204532c02a1ce63d2ad23a5dc2c3aabe1bcb
CodeParl <cybarox@gmail.com>
2021-06-18 11:19:30 -0700

    version 1.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# Codeparl image carousel
+
+## screenshot
+
+![screenshot](screenshot.png)
+
+## About this code
+
+This code example shows how to write a simple image carousel in vanillajs.
+An image carousel is a collection of hidden images in a div element which are shown one at a time by sliding in the current image and stays for some seconds until the next image slides in.
+
+## How to run the code
+
+To run this code example, simply download the code and open the folder and find `index.html` and open it with your web browser.
+
+## Author
+
+This code example was writted by [Codeparl](https://github.com/codeparl)
+
+## More code examples
+
+For more code examples, please checkout [codeparl.com](https://codeparl.com)
+
+## License
+
+Licensed under MIT (https://github.com/codeparl/Codeparl-image-carousel/blob/master/LICENSE).
---
+++ assets/styles.js
@@ -0,0 +1,21 @@
+var styles  ={
+    general:"*{"+
+        "border: 0;"+
+       +"box-sizing: border-box;"
+         +"outline: none;"
+         +"margin: auto;"+
+       "}",
+
+    body:"body{"+
+        "padding: 0;"+
+        "font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"+
+        "font-size: 16px;"+
+        "background-color: rgb(223, 230, 230);"+
+        "} ",
+        
+        main:"main{background-color: #FFF; width: 950px;border-radius: 5px;min-height: 100px;padding: 7px;"+
+        "margin: 10px auto;"+
+        "box-shadow: 1px 1px 4px 0px rgba(0, 0, 0, 0.465);}",
+        h1:" h1{padding: 10px;border-bottom: 2px solid #CCC;color: rgba(0, 0, 0, 0.671);font-weight: 700;font-size: 32px;margin-bottom: 5px;font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;"+
+        "text-shadow: 1px 1px 2px rgba(90, 89, 89, 0.39);letter-spacing: 7px;}"
+};
